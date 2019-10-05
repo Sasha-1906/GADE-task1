6637ba23c4159d3c74518fc776b8b0bec60bcd09
Sasha-1906 <55442527+Sasha-1906@users.noreply.github.com>
2019-10-05 13:37:10 +0200

    Game Engine Class
    Contains a round method that finds the target unit and determines what to do when a units health is below 25%.

---
+++ GameEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_1
+{
+    class GameEngine
+    {
+        public int roundCount=0;
+
+        public void Round(Unit[] unitAr)
+        {
+            roundCount++;
+            for (int i = 0; i < unitAr.Length; i++)
+            {
+                unitAr[i].UnitPosition(unitAr);//got target
+                
+                if (unitAr[i].Health >= unitAr[i].MaxHealth / 4 && unitAr[i].AttackRangeCheck(unitAr))//checked if its in range and above a quarter health
+                {
+                    unitAr[i].Combat(unitAr);//attack
+                }
+                else
+                {
+                    unitAr[i].Move(unitAr);
+                }
+            }
+        }
+    }
+}
