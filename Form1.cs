b0e0bbffb6ad4d11d2e73346e609cd878fdb0a40
Sasha-1906 <55442527+Sasha-1906@users.noreply.github.com>
2019-10-05 13:19:51 +0200

    Units class
    An abstract class with protected variables and abstract methods.
    This is a parent class.

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace PROJECT_1
+{
+    public partial class frmGameEngine : Form
+    {
+        Map map = new Map(15);
+        GameEngine gameEngine = new GameEngine();
+        Timer timer = new Timer();
+
+        public frmGameEngine()
+        {
+            InitializeComponent();
+            DoAgain();
+        }
+
+        public void DoAgain ()
+        {
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(Round);
+        }
+
+        public void Round(Object sender, System.EventArgs e)
+        {
+            gameEngine.Round(map.unit);
+            map.Refresh();
+            lblGameRound.Text = "Round: " + gameEngine.roundCount;
+            map.PopuLbl(lblGameMap);
+            map.PrintInfo(txtUnitUpdate);
+
+            if(map.lastUnit())
+            {
+                timer.Enabled = false;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            map.BattlefieldRndGen();
+            map.PopuLbl(lblGameMap);
+
+            timer.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+        }
+    }
+}
---
+++ Unit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_1
+{
+    public abstract class Unit
+    {
+        protected int x, y, health, maxHealth, speed, attack, attackRange, faction, index;
+        protected char symbol;
+        protected bool isAttacking;
+
+
+        public abstract void Move(Unit[] unitAr);
+        public abstract void Combat(Unit[] unitAr);
+        public abstract bool AttackRangeCheck(Unit[] unitAr);
+        public abstract void UnitPosition(Unit[] unitAr);
+        public abstract void UnitDeath();
+        public abstract string ToString();
+
+        public Unit(int x, int y, int health, int speed,int attack,int attackRange, int faction, char symbol, bool isAttacking)
+        {
+            this.x =x;
+            this.y =y;
+            this.health =health;
+            this.speed = speed;
+            this.attack = attack;
+            this.attackRange = attackRange;
+            this.faction = faction;
+            this.symbol =symbol;
+            this.isAttacking = isAttacking;
+            this.index = 0;
+        }
+
+        public abstract int X { get; set; }
+
+        public abstract int Y { get; set; }
+
+        public abstract int Health { get; set; }
+
+        public abstract int MaxHealth { get;}
+
+        public abstract int Speed { get; set; }
+
+        public abstract int Attack { get; set; }
+
+        public abstract int AttackRange { get; set; }
+
+        public abstract int Faction { get; set; }
+
+        public abstract char Symbol { get; set; }
+
+        public abstract bool IsAttacking { get; set; }
+
+        public abstract int Index { get; set; }
+
+    }
+
+ 
+}
